28aa1932727c03bc25d553039ecf7b11e77aa9cc
Carlos Barron <albertc.barron@gmail.com>
2021-06-08 23:44:51 -0400

    Add files via upload

---
+++ src/photos/Resume.js
@@ -0,0 +1,99 @@
+
+export const myHTML =
+`
+<html>
+<head><meta http-equiv=Content-Type content="text/html; charset=UTF-8">
+<style type="text/css">
+<!--
+span.cls_002{font-family:"Calibri",serif;font-size:11.1px;color:rgb(108,108,108);font-weight:normal;font-style:normal;text-decoration: none}
+div.cls_002{font-family:"Calibri",serif;font-size:11.1px;color:rgb(108,108,108);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_019{font-family:"Calibri",serif;font-size:10.6px;color:rgb(114,114,114);font-weight:normal;font-style:normal;text-decoration: underline}
+div.cls_019{font-family:"Calibri",serif;font-size:10.6px;color:rgb(114,114,114);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_004{font-family:"Calibri",serif;font-size:10.6px;color:rgb(108,108,108);font-weight:normal;font-style:normal;text-decoration: none}
+div.cls_004{font-family:"Calibri",serif;font-size:10.6px;color:rgb(108,108,108);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_005{font-family:"Calibri",serif;font-size:22.0px;color:rgb(108,108,108);font-weight:normal;font-style:normal;text-decoration: none}
+div.cls_005{font-family:"Calibri",serif;font-size:22.0px;color:rgb(108,108,108);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_020{font-family:"Calibri",serif;font-size:10.6px;color:rgb(129,129,129);font-weight:normal;font-style:normal;text-decoration: underline}
+div.cls_020{font-family:"Calibri",serif;font-size:10.6px;color:rgb(129,129,129);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_007{font-family:"Calibri Bold",serif;font-size:13.0px;color:rgb(108,108,108);font-weight:bold;font-style:normal;text-decoration: none}
+div.cls_007{font-family:"Calibri Bold",serif;font-size:13.0px;color:rgb(108,108,108);font-weight:bold;font-style:normal;text-decoration: none}
+span.cls_008{font-family:"Calibri Bold",serif;font-size:12.1px;color:rgb(108,108,108);font-weight:bold;font-style:normal;text-decoration: none}
+div.cls_008{font-family:"Calibri Bold",serif;font-size:12.1px;color:rgb(108,108,108);font-weight:bold;font-style:normal;text-decoration: none}
+span.cls_009{font-family:Arial,serif;font-size:11.1px;color:rgb(108,108,108);font-weight:normal;font-style:normal;text-decoration: none}
+div.cls_009{font-family:Arial,serif;font-size:11.1px;color:rgb(108,108,108);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_010{font-family:Arial,serif;font-size:11.1px;color:rgb(108,108,108);font-weight:normal;font-style:normal;text-decoration: none}
+div.cls_010{font-family:Arial,serif;font-size:11.1px;color:rgb(108,108,108);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_011{font-family:"Calibri Bold",serif;font-size:11.1px;color:rgb(108,108,108);font-weight:bold;font-style:normal;text-decoration: none}
+div.cls_011{font-family:"Calibri Bold",serif;font-size:11.1px;color:rgb(108,108,108);font-weight:bold;font-style:normal;text-decoration: none}
+span.cls_012{font-family:"Calibri Bold Italic",serif;font-size:12.1px;color:rgb(108,108,108);font-weight:bold;font-style:italic;text-decoration: none}
+div.cls_012{font-family:"Calibri Bold Italic",serif;font-size:12.1px;color:rgb(108,108,108);font-weight:bold;font-style:italic;text-decoration: none}
+span.cls_013{font-family:"Calibri",serif;font-size:10.0px;color:rgb(108,108,108);font-weight:normal;font-style:normal;text-decoration: none}
+div.cls_013{font-family:"Calibri",serif;font-size:10.0px;color:rgb(108,108,108);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_014{font-family:"Calibri Bold",serif;font-size:12.1px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+div.cls_014{font-family:"Calibri Bold",serif;font-size:12.1px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+span.cls_015{font-family:"Calibri Bold Italic",serif;font-size:11.1px;color:rgb(108,108,108);font-weight:bold;font-style:italic;text-decoration: none}
+div.cls_015{font-family:"Calibri Bold Italic",serif;font-size:11.1px;color:rgb(108,108,108);font-weight:bold;font-style:italic;text-decoration: none}
+span.cls_016{font-family:"Calibri",serif;font-size:12.1px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+div.cls_016{font-family:"Calibri",serif;font-size:12.1px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_021{font-family:"Calibri",serif;font-size:12.1px;color:rgb(114,114,114);font-weight:normal;font-style:normal;text-decoration: underline}
+div.cls_021{font-family:"Calibri",serif;font-size:12.1px;color:rgb(114,114,114);font-weight:normal;font-style:normal;text-decoration: none}
+-->
+</style>
+</head>
+<body>
+<div>
+<div style="position:absolute;left:0px;top:0px">
+<img src="./background1.jpg" width=612 height=792></div>
+<div style="position:absolute;left:38.88px;top:33.36px" class="cls_002"><span class="cls_002">Winston Salem, NC</span></div>
+<div style="position:absolute;left:475.27px;top:33.36px" class="cls_002"><span class="cls_002">(336)-450-9654</span></div>
+<div style="position:absolute;left:38.88px;top:44.16px" class="cls_019"><span class="cls_019"> </span><A HREF="http://www.github.com/guadarrama">www.github.com/guadarrama</A> </div>
+<div style="position:absolute;left:418.87px;top:44.16px" class="cls_004"><span class="cls_004">C.Alberto.Barron@gmail.com</span></div>
+<div style="position:absolute;left:213.05px;top:39.48px" class="cls_005"><span class="cls_005">CARLOS BARRON</span></div>
+<div style="position:absolute;left:414.43px;top:62.04px" class="cls_020"><span class="cls_020"> </span><A HREF="https://guadarrama.github.io/">https://Guadarrama.GitHub.io</A> </div>
+<div style="position:absolute;left:36.00px;top:96.62px" class="cls_007"><span class="cls_007">EDUCATION</span></div>
+<div style="position:absolute;left:36.00px;top:113.78px" class="cls_008"><span class="cls_008">Arlington, VA</span></div>
+<div style="position:absolute;left:257.81px;top:113.78px" class="cls_008"><span class="cls_008">Coding Dojo</span></div>
+<div style="position:absolute;left:399.91px;top:113.78px" class="cls_008"><span class="cls_008">October 2019 - January 2020</span></div>
+<div style="position:absolute;left:54.00px;top:129.02px" class="cls_009"><span class="cls_009">•</span><span class="cls_010"> </span><span class="cls_011">  Programming Coursework: </span><span class="cls_002">Algorithms, Data Structures, Object Oriented Programming. C# ASP.NET Entity</span></div>
+<div style="position:absolute;left:72.02px;top:142.46px" class="cls_002"><span class="cls_002">Framework Core, ReactJS, ExpressJS, MongoDB, Python, Django, Flask.</span></div>
+<div style="position:absolute;left:36.00px;top:169.34px" class="cls_008"><span class="cls_008">Dobson, NC</span></div>
+<div style="position:absolute;left:224.93px;top:169.34px" class="cls_008"><span class="cls_008">Surry Community College</span></div>
+<div style="position:absolute;left:421.39px;top:169.34px" class="cls_008"><span class="cls_008">August 2014 - June 2016</span></div>
+<div style="position:absolute;left:54.00px;top:184.46px" class="cls_009"><span class="cls_009">•</span><span class="cls_010"> </span><span class="cls_011">  Completion:</span><span class="cls_002"> Associate is Science and Associate in Arts (</span><span class="cls_011">GPA: 3.7</span><span class="cls_002">)</span></div>
+<div style="position:absolute;left:54.00px;top:198.50px" class="cls_009"><span class="cls_009">•</span><span class="cls_010"> </span><span class="cls_011">  Relevant Coursework: </span><span class="cls_002">Fundamentals of C#, Calculus (1, 2 & 3), Differential Equations, Linear Algebra</span></div>
+<div style="position:absolute;left:36.00px;top:222.74px" class="cls_007"><span class="cls_007">EMPLOYMENT</span></div>
+<div style="position:absolute;left:36.00px;top:240.02px" class="cls_012"><span class="cls_012">Helpdesk, Independent Contractor</span></div>
+<div style="position:absolute;left:253.13px;top:240.02px" class="cls_012"><span class="cls_012">Self Employed</span></div>
+<div style="position:absolute;left:446.11px;top:240.02px" class="cls_012"><span class="cls_012">May 2016 - present</span></div>
+<div style="position:absolute;left:36.00px;top:254.93px" class="cls_013"><span class="cls_013">Worked with clients to provide solutions to computer equipment in small business environments. Including installation and</span></div>
+<div style="position:absolute;left:36.00px;top:267.05px" class="cls_013"><span class="cls_013">configuration of LAN along with their respective hardware and software. Prepared and updated documentation required.</span></div>
+<div style="position:absolute;left:36.00px;top:279.29px" class="cls_013"><span class="cls_013">Provided support as required on a 24/7 basis.</span></div>
+<div style="position:absolute;left:36.00px;top:291.29px" class="cls_008"><span class="cls_008">Full Stack Developer</span></div>
+<div style="position:absolute;left:257.81px;top:291.29px" class="cls_008"><span class="cls_008">Coding Dojo</span></div>
+<div style="position:absolute;left:399.91px;top:291.29px" class="cls_008"><span class="cls_008">October 2019 - January 2020</span></div>
+<div style="position:absolute;left:36.00px;top:306.17px" class="cls_013"><span class="cls_013">Dedicated over 1,000 hours in a 3 month intensive hands- on training to develop dozens of web applications using Python, C#</span></div>
+<div style="position:absolute;left:36.00px;top:318.41px" class="cls_013"><span class="cls_013">and Javascript. Using a multitude of frameworks such as Flask, Django, ASP.NET Core, Entity Framework and ReactJS. Created</span></div>
+<div style="position:absolute;left:36.00px;top:330.53px" class="cls_013"><span class="cls_013">user friendly sites with CRUD operations and RESTful routes with API integration. Collaborated with colleagues to implement</span></div>
+<div style="position:absolute;left:36.00px;top:342.77px" class="cls_013"><span class="cls_013">both front-end and back-end solutions to a wide array of unique challenges and wireframe specifications.</span></div>
+<div style="position:absolute;left:36.00px;top:365.57px" class="cls_007"><span class="cls_007">SOFTWARE PROJECTS</span></div>
+<div style="position:absolute;left:36.00px;top:382.85px" class="cls_014"><span class="cls_014">Travel Planner</span></div>
+<div style="position:absolute;left:54.00px;top:397.49px" class="cls_015"><span class="cls_015">Developed using Python, Django, SQLite, HTML, CSS - Deployed using AWS(EC2)</span></div>
+<div style="position:absolute;left:54.00px;top:410.81px" class="cls_002"><span class="cls_002">Application allows a multitude of registered users to coordinate travel plans and join travel itineraries via</span></div>
+<div style="position:absolute;left:54.00px;top:424.27px" class="cls_002"><span class="cls_002">SQLite connections and user friendly web-interface</span></div>
+<div style="position:absolute;left:54.00px;top:437.71px" class="cls_002"><span class="cls_002">Featuring login validations, bycript password hashing, sessions, and various schema relationships</span></div>
+<div style="position:absolute;left:36.00px;top:451.15px" class="cls_014"><span class="cls_014">Tool Shop (</span><span class="cls_016">Link: </span><A HREF="http://54.89.14.210/">http://54.89.14.210/)</A> </span></div>
+<div style="position:absolute;left:54.00px;top:465.79px" class="cls_015"><span class="cls_015">Developed using C#, ASP.NET Core, Entity Framework, HTML, CSS and Bootstrap - Deployed using AWS(EC2)</span></div>
+<div style="position:absolute;left:54.00px;top:479.23px" class="cls_002"><span class="cls_002">Application allows users to create items to buy and sell. Includes item checkout, purchase history and</span></div>
+<div style="position:absolute;left:54.00px;top:492.67px" class="cls_002"><span class="cls_002">shopping cart functionality. Also features login and password validations, bcrypt password hashing, session,</span></div>
+<div style="position:absolute;left:54.00px;top:506.11px" class="cls_002"><span class="cls_002">one-to-many and many-to-many schema relationships.</span></div>
+<div style="position:absolute;left:36.00px;top:519.55px" class="cls_014"><span class="cls_014">Pet Shelter(</span><span class="cls_016">Link: </span><A HREF="http://174.129.104.40/pets">http://174.129.104.40/pets</A> )</span></div>
+<div style="position:absolute;left:54.00px;top:534.19px" class="cls_011"><span class="cls_011">Developed using Javascript; ReactJS, NodeJS, MongoDB, Mongoose, HTML, CSS and Bootstrap - Deployed</span></div>
+<div style="position:absolute;left:54.00px;top:547.63px" class="cls_011"><span class="cls_011">using AWS(EC2)</span></div>
+<div style="position:absolute;left:54.00px;top:561.07px" class="cls_002"><span class="cls_002">Application allows users to add pets to a shelter, adopt or like a pet. Features validations, sorting, and one-to-</span></div>
+<div style="position:absolute;left:54.00px;top:574.51px" class="cls_002"><span class="cls_002">one relationships.</span></div>
+</div>
+
+</body>
+</html>
+
+`;
